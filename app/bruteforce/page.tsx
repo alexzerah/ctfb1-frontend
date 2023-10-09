4e8312e00fcee63aa93c3ed4f1ab313d407c7a5d
Alex Zerah <alex.zerah@gmail.com>
2023-10-09 16:10:47 +0200

    fix

--- app/bruteforce/page.tsx
+++ app/bruteforce/page.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link'
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            Vous devez tout d'abord :
-            <Link href={'/bruteforce/create'}>Créer votre code équipe</Link>
+            Vous devez :
+            <Link href="/bruteforce/create">Créer votre code équipe</Link>
 
         </main>
     )
--- app/xss/page.tsx
+++ app/xss/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
             <div className="flex flex-col flex-wrap justify-center max-w-4xl mt-6 sm:w-full">
                 <div className="flex flex-row items-center w-1/2 p-4">
                     {Array.from({ length: 10 }).map((_, index) => (
-                        <div className="flex flex-col items-center justify-center w-1/2 p-4">
+                        <div key={index} className="flex flex-col items-center justify-center w-1/2 p-4">
                             <select key={index} className="bg-blue-700 p-2.5 text-2xl mt-4">
                                 <option value="1">1</option>
                                 <option value="2">2</option>
