8798fb6456f22570fc483c7cbfcdfd2b405c1111
Alex Zerah <alex.zerah@gmail.com>
2023-10-09 16:23:18 +0200

    name

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1 className="text-6xl font-bold text-center">Welcome to <a href="https://nextjs.org">Next.js!</a></h1>
+        <h1 className="text-6xl font-bold text-center">Cyber management scool CTF B1</h1>
         16 selects html avec 16 possibilites de choix
         <div className="flex flex-wrap justify-center max-w-4xl mt-6 sm:w-full">
             <div className="flex flex-col items-center justify-center w-1/2 p-4">
